bed747cc0c70eb080c01aaa15200d7c407f90b3a
Ruslan Suvorov <ruslanvs@yahoo.com>
2020-05-17 22:31:00 -0700

    Removing comments

--- lib/main.dart
+++ lib/main.dart
@@ -11,14 +11,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Startup Name Generator',
       home: RandomWords(),
-      // home: Scaffold(
-      //   appBar: AppBar(
-      //     title: const Text('Welcome to Flutter'),
-      //   ),
-      //   body: Center(
-      //     child: RandomWords(),
-      //   ),
-      // ),
     );
   }
 }
@@ -30,25 +22,13 @@ class RandomWordsState extends State<RandomWords> {
   Widget _buildSuggestions() {
     return ListView.builder(
       padding: const EdgeInsets.all(16),
-      // The itemBuilder callback is called once per suggested
-      // word pairing, and places each suggestion into a ListTile
-      // row. For even rows, the function adds a ListTile row for
-      // the word pairing. For odd rows, the function adds a
-      // Divider widget to visually separate the entries. Note that
-      // the divider may be difficult to see on smaller devices.
       itemBuilder: (BuildContext _context, int i) {
-        // Add a one-pixel-high divider widget before each row
-        // in the ListView.
         if (i.isOdd) {
           return Divider();
         }
 
-        // The syntax "i ~/ 2" divides i by 2 and returns an
-        // integer result.
-        // For example: 1, 2, 3, 4, 5 becomes 0, 1, 1, 2, 2.
-        // This calculates the actual number of word pairings
-        // in the ListView,minus the divider widgets.
         final int index = i ~/ 2;
+
         // If you've reached the end of the available word
         // pairings...
         if (index >= _suggestions.length) {
